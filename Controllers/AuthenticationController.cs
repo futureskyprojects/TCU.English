a4e72162b5701cbccec9ec2a34f4685ab2176e2e
Nghia Nguyen <projects.futuresky@gmail.com>
2020-09-24 07:52:25 +0700

    tối giản using

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using TCU.English.Models;
 using TCU.English.Models.DataManager;
 using TCU.English.Models.Repository;
