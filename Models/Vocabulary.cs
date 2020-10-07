9596523d9cf63147a35163daadb29bdf51029195
Nghia Nguyen <projects.futuresky@gmail.com>
2020-10-07 08:08:23 +0700

    Tạo model topic và từ vựng

---
+++ Models/Vocabulary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCU.English.Models
+{
+    public class Vocabulary : BaseEntity
+    {
+        // Từ thuộc topic gì
+        public int TopicId { get; set; }
+        public string Word { get; set; }
+        public string Spelling { get; set; }
+        public string TypeOfWord { get; set; }
+        public string Use { get; set; }
+
+        // Dẫn khóa đến topic
+        public virtual Topic Topic { get; set; }
+    }
+}
