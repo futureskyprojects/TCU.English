3b06901c755c244e7d36cd809778a9001474108e
Nguyễn Trọng Nghĩa <projects.futuresky@gmail.com>
2020-10-08 16:50:59 +0700

    khởi tạo phần hiển thị khung nhìn cho việc thêm mới từ vựng

---
+++ Controllers/Dictionary/DictionaryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCU.English.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
